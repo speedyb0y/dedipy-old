1936e840d320551ee2d6f91bf65352ade4be70b0
Willian B <speedyb0y@users.noreply.github.com>
2020-08-25 22:23:10 -0300

    Update and rename test.sh to run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# EXECUTAR COMO ROOT
+
+set -e
+
+rm -f -- malloc-buffer master slave malloc.o libmalloc.so
+
+gcc -Wall -Werror -march=native -O0 -g -c -fpic malloc.c
+gcc -Wall -Werror -march=native -O0 -g -shared -o libmalloc.so malloc.o
+
+gcc -Wall -Werror -march=native -O0 -g -fwhole-program -DLIB_MALLOC_PATH=\"$(pwd)/libmalloc.so\" master.c -o master
+
+gcc -Wall -Werror -march=native -O0 -g -fwhole-program slave.c -o slave
+
+# sudo  mount -t hugetlbfs nodev /opt/
+
+fallocate -l $[2*1024*1024*1024] malloc-buffer
+
+sudo ./master
+
+echo $?
