501a22dfa367160cb41072f7a24c56a60c942d9f
Willian B <speedyb0y@users.noreply.github.com>
2020-08-27 20:37:46 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-rm -f -- malloc-buffer master slave malloc.o libmalloc.so
+rm -f -- malloc-buffer master slave malloc.o libmalloc.so LOG.*
 
-gcc -Wall -Werror -march=native -O0 -g -c -fpic malloc.c
-gcc -Wall -Werror -march=native -O0 -g -shared -o libmalloc.so malloc.o
+gcc -Wfatal-errors -Wall -Werror -march=native -O0 -g -c -fpic malloc.c
+gcc -Wfatal-errors -Wall -Werror -march=native -O0 -g -shared -o libmalloc.so malloc.o
 
-gcc -Wall -Werror -march=native -O0 -g -fwhole-program -DLIB_MALLOC_PATH=\"$(pwd)/libmalloc.so\" master.c -o master
+gcc -Wfatal-errors -Wall -Werror -march=native -O0 -g -fwhole-program -DLIB_MALLOC_PATH=\"$(pwd)/libmalloc.so\" master.c -o master
 
-gcc -Wall -Werror -march=native -O0 -g -fwhole-program slave.c -o slave
+gcc -Wfatal-errors -Wall -Werror -march=native -O0 -g -fwhole-program slave.c -o slave
 
 # sudo  mount -t hugetlbfs nodev /opt/
 
-fallocate -l $[2*1024*1024*1024] malloc-buffer
+fallocate -l $[128*1024*1024] malloc-buffer
 
-sudo ./master
+sudo strace --follow-forks --output-separately -o LOG ./master
 
 echo $?
