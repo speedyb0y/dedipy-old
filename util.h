40e98c8c9e229dbaffaaebbe345131cdac1d95a0
Willian B <speedyb0y@users.noreply.github.com>
2020-08-27 20:42:46 -0300

    Update util.h

--- util.h
+++ util.h
@@ -7,6 +7,18 @@
 
 typedef unsigned int uint;
 
+typedef unsigned long long int uintll;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
 typedef uint64_t u64;
 
-#define WRITESTR(str) write(STDOUT_FILENO, str "\n", sizeof(str))
+#define DBGPRINT(str) write(STDOUT_FILENO, str "\n", sizeof(str))
+
+static inline u64 rdtsc(void) {
+    uint lo;
+    uint hi;
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
+}
