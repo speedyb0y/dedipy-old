75c310c1d7ddb8ebb43913c6855434fd163fd4d9
Willian B <speedyb0y@users.noreply.github.com>
2020-08-31 01:54:22 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,17 +3,17 @@
 set -e
 
 BUFF_PATH="/tmp/malloc-buffer"
-BUFF_SIZE=$[1024*1024*1024]
+BUFF_SIZE=$[4*1024*1024*1024+16*1024*1024]
 
 rm -f -- ${BUFF_PATH}
 rm -f -- master slave malloc.o libmalloc.so LOG.*
 
-gcc -DDEBUG=0 -DBUFF_PATH=\"${BUFF_PATH}\" -DBUFF_SIZE=${BUFF_SIZE} -Wfatal-errors -Wall -Werror -march=native -O0 -g -c -fpic malloc.c
-gcc -DDEBUG=0 -DBUFF_PATH=\"${BUFF_PATH}\" -DBUFF_SIZE=${BUFF_SIZE} -Wfatal-errors -Wall -Werror -march=native -O0 -g -shared -o libmalloc.so malloc.o
+gcc -DDEBUG=1 -DBUFF_PATH=\"${BUFF_PATH}\" -DBUFF_SIZE=${BUFF_SIZE} -Wfatal-errors -Wall -Werror -march=native -O0 -g -c -fpic malloc.c
+gcc -DDEBUG=1 -DBUFF_PATH=\"${BUFF_PATH}\" -DBUFF_SIZE=${BUFF_SIZE} -Wfatal-errors -Wall -Werror -march=native -O0 -g -shared -Wl,-init,ms_init -o libmalloc.so malloc.o
 
-gcc -DDEBUG=0 -DBUFF_PATH=\"${BUFF_PATH}\" -DBUFF_SIZE=${BUFF_SIZE} -Wfatal-errors -Wall -Werror -march=native -O0 -g -fwhole-program -DLIB_MALLOC_PATH=\"$(pwd)/libmalloc.so\" master.c -o master
+gcc -DDEBUG=1 -DBUFF_PATH=\"${BUFF_PATH}\" -DBUFF_SIZE=${BUFF_SIZE} -Wfatal-errors -Wall -Werror -march=native -O0 -g -fwhole-program -DLIB_MALLOC_PATH=\"$(pwd)/libmalloc.so\" master.c -o master
 
-gcc -DDEBUG=0 -DBUFF_PATH=\"${BUFF_PATH}\" -DBUFF_SIZE=${BUFF_SIZE} -Wfatal-errors -Wall -Werror -march=native -O0 -g -fwhole-program slave.c -o slave
+gcc -DDEBUG=1 -DBUFF_PATH=\"${BUFF_PATH}\" -DBUFF_SIZE=${BUFF_SIZE} -Wfatal-errors -Wall -Werror -march=native -O0 -g -fwhole-program slave.c -o slave
 
 # sudo  mount -t hugetlbfs nodev /opt/
 
