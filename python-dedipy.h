0506b20b54a65640cb9c6b9a45ba1560c248b3e9
Willian B <speedyb0y@users.noreply.github.com>
2020-09-01 16:08:49 -0300

    Create python-dedipy.h

---
+++ python-dedipy.h
@@ -0,0 +1,11 @@
+/*
+
+*/
+
+void* dedipy_calloc (size_t n, size_t size_);
+void dedipy_free (void* const data);
+void* dedipy_malloc (size_t size_);
+void* dedipy_realloc (void* const data_, const size_t dataSizeNew_);
+void* dedipy_reallocarray (void *ptr, size_t nmemb, size_t size);
+void dedipy_test (void);
+void dedipy_main (void);
