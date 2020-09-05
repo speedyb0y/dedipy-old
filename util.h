fd764d012e7fa85abcf67dd857d8f1c0fcb4f2a6
Willian B <speedyb0y@users.noreply.github.com>
2020-09-05 04:08:38 -0300

    Update util.h

--- util.h
+++ util.h
@@ -2,6 +2,8 @@
 
 */
 
+#undef assert
+
 #ifndef DEBUG
 #define DEBUG 0
 #endif
@@ -39,7 +41,7 @@ typedef uint64_t u64;
 // TODO: FIXME: name fmt \n, nameargs, ##__VA_ARGS__
 #if DEBUG
 #define DBGPRINT(str) write(STDOUT_FILENO, __FILE__ ":- " str "\n", sizeof(__FILE__ ":- " str))
-#define dbg(x, ...) ({ char b[4096]; write(STDERR_FILENO, b, snprintf(b, sizeof(b), "%20s %-30s %5d " DBG_PREPEND x "\n", __FILE__, __func__, __LINE__ _DBG_PREPEND_ARGS, ##__VA_ARGS__)); })
+#define dbg(fmt, ...) ({ char b[8192]; write(STDERR_FILENO, b, (size_t)snprintf(b, sizeof(b), "%20s %-30s %5d " DBG_PREPEND fmt "\n", __FILE__, __func__, __LINE__ _DBG_PREPEND_ARGS, ##__VA_ARGS__)); })
 #else
 #define DBGPRINT(str) ({ })
 #define dbg(fmt, ...) ({ })
@@ -64,11 +66,8 @@ static inline u64 rdtsc(void) {
     return ((uint64_t)hi << 32) | lo;
 }
 
-// TODO: FIXME: builtion choose expression -> int 0 / 1
-
-
 // TODO: FIXME: usando o builtin choose expression, se for constante expression, verificar em compile time =]
-#define ASSERT(condition) ({ if (!(condition)) { write(STDERR_FILENO, "\n" __FILE__ ":" LINE(__LINE__) " ASSERT FAILED: " #condition "\n", sizeof("\n" __FILE__ ":" LINE(__LINE__) " ASSERT FAILED: " #condition "\n")); abort(); } })
+#define assert(condition) ({ if (!(condition)) { write(STDERR_FILENO, "\n" __FILE__ ":" LINE(__LINE__) " ASSERT FAILED: " #condition "\n", sizeof("\n" __FILE__ ":" LINE(__LINE__) " assert FAILED: " #condition "\n")); abort(); } })
 
 // TODO: FIXME: :S
 #define abort_group() ({ kill(0, SIGABRT); kill(0, SIGKILL); abort(); })
@@ -76,34 +75,23 @@ static inline u64 rdtsc(void) {
 #define fatal(reason)       ({ write(2, "FATAL: " reason "\n", sizeof("FATAL: " reason)); abort(); })
 #define fatal_group(reason) ({ write(2, "FATAL: " reason "\n", sizeof("FATAL: " reason)); abort_group(); })
 
-/*
- TODO: FIXME: forçar compilacao a falhar :/
-#define _CHUNK(t, c) \
-    __builtin_choose_expr(( \
-        __builtin_types_compatible_p(typeof(c), Chunk*) || \
-        __builtin_types_compatible_p(typeof(c), ChunkUnkn*) || \
-        __builtin_types_compatible_p(typeof(c), ChunkFree*) || \
-        __builtin_types_compatible_p(typeof(c), ChunkUsed*) \
-        ), (t*)(c), (long double)1 )
-*/
-
-#define _FORCE(t, c) __builtin_choose_expr(__builtin_types_compatible_p(t, typeof(c)), (c), (void)0)
-
-#define FORCE_U64(c) _FORCE(u64, c)
-
-// typeof(sizeof(int))
-
-static inline int in_buff (const void* const thing, const u64 thingSize, const void* const buff, const u64 buffSize2)
-    { return buff <= thing && (thing + thingSize) <= (buff + buffSize2); }
+static inline int in_mem (const void* const mem, const u64 memSize, const void* const mem2, const u64 memSize2)
+    { return mem2 <= mem && (mem + memSize) <= (mem2 + memSize2); }
 
 // E É UM ELEMENTO DA ARRAY A, DE N ELEMENTOS DE TAMANHO S
 static inline int is_element_of_array (const void* const e, const void* const a, const u64 n, const u64 s)
     { return (a <= e) && (e < (a + n*s)) && ((uintptr_t)e % sizeof(s)) == 0; }
 
 // PROTEGE A FUNCAO
-#define is_element_of_array(e, a, n) __builtin_choose_expr ( builtin_types_compatible(typeof(e), typeof(a)), is_element_of_array((e), (a), (n), sizeof(*a)), (void)0 )
+//#define is_element_of_array(e, a, n) __builtin_choose_expr ( __builtin_types_compatible_p(typeof(e), typeof(*a)), is_element_of_array((e), (a), (n), sizeof(*a)), (void)0 )
+#define is_element_of_array(e, a, n) is_element_of_array(e, a, n, n*sizeof(*a))
 
-// TODO: FIXME: tem que confirmar que os tipos são os mesmos: e
-#define assert_is_element_of_array(e, a, n) ASSERT( is_element_of_array(e, a, n) )
+// ASSIM COMO UMA FUNCAO, EXECUTA OS ARGUMENTOS ANTES DE EXECUTAR!!!
+#define is_from_to(min, value, max)      ({ typeof(value) __value;  ((min) <= (__value = (value)) && __value <= (max)); })
+#define is_from_between(min, value, max) ({ typeof(value) __value;  ((min) <  (__value = (value)) && __value <  (max)); })
 
-#define assert_in_buff(thing, thingSize, buff, buffSize) ASSERT(in_buff((thing), (thingSize), (buff), (buffSize2)))
+typedef int bool;
+
+#define is_aligned(addr, alignment) (!!((addr) % (alignment)))
+
+#define is_aligned_ptr(addr, alignment) (!!((uintptr_t)(addr) % (alignment)))
